29bb9da744d22fe27e81d96a2fe4ff76577b3e9d
Mattia <locastromattia@hotmail.it>
2019-11-19 14:13:39 +0100

    class methods

--- datianagrafici/src/main/java/_283095/datianagrafici/Amministratore.java
+++ datianagrafici/src/main/java/_283095/datianagrafici/Amministratore.java
@@ -6,12 +6,18 @@ public class Amministratore extends Impiegato
 {
   String email;
   String psw;
-  
+
   Amministratore(String _name, String _surname, String _taxCode,
-      String _hqAddress, Date _start, Date _end,String _email, String _psw)
+      String _hqAddress, Date _start, Date _end, String _email, String _psw)
   {
-    super(_name, _surname, _taxCode, _hqAddress, "Amministratore", _start, _end);
+    super(_name, _surname, _taxCode, _hqAddress, "Amministratore", _start,
+        _end);
     email = _email;
     psw = _psw;
+  }
+
+  public String search(String _job)
+  {
+    return "Search," + _job;
   }
 }
--- datianagrafici/src/main/java/_283095/datianagrafici/Funzionario.java
+++ datianagrafici/src/main/java/_283095/datianagrafici/Funzionario.java
@@ -11,8 +11,43 @@ public class Funzionario extends Impiegato
   Funzionario(String _name, String _surname, String _taxCode, String _hqAddress,
       Date _start, Date _end, String _email, String _psw)
   {
-    super(_name, _surname, _taxCode, _hqAddress, "Funzionario",_start, _end);
+    super(_name, _surname, _taxCode, _hqAddress, "Funzionario", _start, _end);
     email = _email;
-    psw = _psw;  
+    psw = _psw;
+  }
+
+  public Impiegato InsertDipendente(String _name, String _surname,
+      String _taxCode, String _hqAddress, String _job, Date _start, Date _end)
+  {
+    return new Impiegato(_name, _surname, _taxCode, _hqAddress, _job, _start,
+        _end);
+  }
+
+  public Impiegato ModifyDipendente(Impiegato _myImpiegato, String _name,
+      String _surname, String _taxCode, String _hqAddress, String _job,
+      Date _start, Date _end)
+  {
+    if (_name != "")
+      _myImpiegato.name = _name;
+
+    if (_surname != "")
+      _myImpiegato.surname = _surname;
+
+    if (_taxCode != "")
+      _myImpiegato.taxCode = _taxCode;
+
+    if (_hqAddress != "")
+      _myImpiegato.hqAddress = _hqAddress;
+
+    if (_job != "")
+      _myImpiegato.job = _job;
+
+    if (_start != _myImpiegato.start)
+      _myImpiegato.start = _start;
+
+    if (_start != _myImpiegato.end)
+      _myImpiegato.end = _end;
+
+    return _myImpiegato;
   }
 }
